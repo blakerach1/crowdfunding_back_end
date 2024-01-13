64181875f5273dcfb5af9ed8d82c44b2ec31f5bd
Rachel Blake <blakerach1@gmail.com>
2024-01-13 15:19:56 +0800

    permission on pledge editing needs to be completed

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -10,6 +10,23 @@ class PledgeSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class PledgeDetailSerializer(serializers.ModelSerializer):
+    supporter = serializers.ReadOnlyField(source='supporter.id')
+
+    class Meta:
+        model = Pledge
+        fields = '__all__'
+
+    def update(self, instance, validated_data):
+        instance.amount = validated_data('amount', instance.amount)
+        instance.comment = validated_data('comment', instance.comment)
+        instance.anonymous = validated_data('anonymous', instance.anonymous)
+        instance.project_id = validated_data('project_id', instance.project_id)
+        instance.supporter_id = validated_data('supporter_id', instance.supporter_id)
+        instance.save()
+        return instance
+
+
 class ProjectSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.id')
 
