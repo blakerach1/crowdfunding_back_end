1504a72ac6ec1b7b8cfc2e474156defe806bd791
Rachel Blake <blakerach1@gmail.com>
2024-01-13 14:31:02 +0800

    connected apps to the django admin panel

--- crowdfunding/projects/admin.py
+++ crowdfunding/projects/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Project, Pledge
+from users.models import CustomUser
 
 # Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Project, ProjectAdmin)
+
+
+class PledgeAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Pledge, PledgeAdmin)
+
+
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(CustomUser, UserAdmin)
