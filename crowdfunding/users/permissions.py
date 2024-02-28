1e7ed63cd66cbe1286c16988820eb7acccdf1776
Rachel Blake <blakerach1@gmail.com>
2024-02-28 23:10:33 +0800

    minor update to permissions

--- crowdfunding/users/permissions.py
+++ crowdfunding/users/permissions.py
@@ -4,4 +4,5 @@ class IsUserOrReadOnlyUser(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj == request.user
+        return obj.id == request.user
+
